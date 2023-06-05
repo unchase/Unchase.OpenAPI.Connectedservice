9f76695988090b9d6ceee3d3063ceca9a4c98856
Andreas Böhm <aboehm@matherm.de>
2023-06-05 11:21:26 +0200

    chore: Revert removal of image for the "Report a bug" button
    Some workarounds are required to be implemented, to load the image in a shared project and show it in WPF designer (see SharedImageSourceExtension).

--- src/Unchase.OpenAPI.Connectedservice.Shared/Common/ExtensionsHelper.cs
+++ src/Unchase.OpenAPI.Connectedservice.Shared/Common/ExtensionsHelper.cs
@@ -16,5 +16,22 @@ namespace Unchase.OpenAPI.ConnectedService.Common
                 stackPanel.Visibility = Visibility.Collapsed;
             }
         }
+
+        internal static bool TryDowncastToFEorFCE(this DependencyObject d, out FrameworkElement fe, out FrameworkContentElement fce)
+        {
+            fe = null;
+            fce = null;
+            if (d is FrameworkElement fe2)
+            {
+                fe = fe2;
+                return true;
+            }
+            else if (d is FrameworkContentElement fce2)
+            {
+                fce = fce2;
+                return true;
+            }
+            return false;
+        }
     }
 }
